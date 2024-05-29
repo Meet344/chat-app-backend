4c14f62a79f6c4172818513263e271975c544285
MT <meetprakash342004@gmail.com>
2024-05-29 16:10:37 +0530

    Jay mataji

---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+	try {
+		const loggedInUserId = req.user._id;
+		// const loggedInUserId = "6655ca1a15b072daaf276f15";
+
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const getUserForSidebar = async (req, res) => {
+	try {
+		const loggedInUserId = req.params.id;
+		// const loggedInUserId = "6655ca1a15b072daaf276f15";
+
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
